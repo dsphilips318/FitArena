0b4c14a2a75bf0edf1b9e97052bba71b28f8ae02
dsphilips318 <dsphilips18@gmail.com>
2022-01-14 11:35:12 -0500

    Initial Commit

---
+++ FitnessHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitnessHub
+//
+//  Created by CS on 1/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FitnessHub/FitnessHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessHubApp.swift
+//  FitnessHub
+//
+//  Created by CS on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
